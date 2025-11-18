142901eb5ed8316e92f487cd0f87bda8cadc0aff
unknown <Cetecom@SJAPCA03LC0717.duoc.acad>
2025-11-18 13:31:41 -0300

    Base hecha

---
+++ src/main/java/com/sintaxis/apimascotas/ApimascotasApplication.java
@@ -0,0 +1,13 @@
+package com.sintaxis.apimascotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApimascotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApimascotasApplication.class, args);
+	}
+
+}
